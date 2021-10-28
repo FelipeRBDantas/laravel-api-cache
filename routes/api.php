d4a5d0fb26d645f776ac078da7f661a4dead3ceb
Felipe Dantas <feliperbdantas@outlook.com>
2021-10-28 16:10:07 -0300

    Tests Course Finished

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,12 @@ use App\Http\Controllers\Api\{
     CourseController, ModuleController, LessonController
 };
 
-Route::apiResource('/modules/{module}/lessons', LessonController::class);
-
-Route::apiResource('/courses/{course}/modules', ModuleController::class);
-
 Route::put('/courses/{course}', [CourseController::class, 'update']);
 Route::delete('/courses/{identify}', [CourseController::class, 'destroy']);
 Route::get('/courses/{identify}', [CourseController::class, 'show']);
 Route::post('/courses', [CourseController::class, 'store']);
 Route::get('/courses', [CourseController::class, 'index']);
+
+Route::apiResource('/courses/{course}/modules', ModuleController::class);
+
+Route::apiResource('/modules/{module}/lessons', LessonController::class);
